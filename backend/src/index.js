0a8d535798cf78f1f1fddf1c68a00f201e816c07
redyy <pedroredyypt@gmail.com>
2020-03-28 02:48:21 +0000

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,10 @@ const routes = require('./routes');
 
 const app = express();
 
+//app.use(cors({
+ // origin: 'http://meuapp.com'
+//}))
+
 app.use(cors())
 app.use(express.json());
 app.use(routes);
