8b3a8843be47063947ec5252adcc7bdd18ee60eb
redyy <pedroredyypt@gmail.com>
2020-03-28 19:19:57 +0000

    add delay in login

---
+++ frontend/src/components/noti.js
@@ -0,0 +1,2 @@
+import { store } from 'react-notifications-component';
+
--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
 import api from '../../services/api'
+
+import ReactNotification from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
+import notification from '../../components/noti'
 import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
-
+import { ClipLoader } from 'react-spinners';
 import './styles.css'
 
 import heroesImg from '../../assets/heroes.png';
@@ -11,6 +15,7 @@ import logoImg from '../../assets/logo.svg';
 export default function Logon(){
   const [id, setId] = useState('');
   const history = useHistory();
+  const [success, setSuccess] = useState(false);
 
   async function handleLogin(e){
     e.preventDefault();
@@ -19,15 +24,18 @@ export default function Logon(){
       const res = await api.post('sessions', { id });
       localStorage.setItem('ongID', id);
       localStorage.setItem('ongName', res.data.name);
-      history.push('/profile');
-
+      setSuccess(true)
+      setTimeout(() => {
+        history.push('/profile');
+      }, 2000);
     } catch (e){
-      alert(e);
+      alert(e)
     }
   }
 
   return(
     <div className="logon-container">
+    <ReactNotification />
       <section className="form">
         <img src={logoImg} alt="Be The Hero"/>
       
@@ -39,7 +47,17 @@ export default function Logon(){
           value={id}
           onChange={e => setId(e.target.value)}
           />
+          {!success &&
           <button className="button" type="submit">Entrar</button>
+          }
+          {success &&
+          <button className="button" style={{ cursor: 'default', opacity: 0.8 }} disabled><ClipLoader
+          sizeUnit={"px"}
+          size={40}
+          color={'#fff'}
+          loading={success}
+          /></button>
+          }
         
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#E02041" />  
