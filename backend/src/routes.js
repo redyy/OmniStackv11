14d63958035f1d64e730e07cd5c273e9ddfdf022
redyy <pedroredyypt@gmail.com>
2020-03-31 14:46:05 +0100

    backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -20,7 +20,7 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.string().required().min(12).max(12),
+    whatsapp: Joi.string().required().min(9).max(12),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   })
